1b48cdec2ac32a564f709b9a27f6e2c11ebd9133
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2025-08-16 12:12:09 +0530

    feat: add data-line attribute to html elements in parsing

--- src/MarkdownParser.tsx
+++ src/MarkdownParser.tsx
@@ -78,7 +78,9 @@ class MarkdownParser {
       let isULRunning = false
       let isOLRunning = false
 
-      for (const line of html) {
+      for (let index = 0; index < html.length; index++) {
+         const line = html[index]
+
          // code block parsing
          const codeBlockExec = /\`{3}([\s\S]*?)\`{3}/gm.exec(line)
          if (codeBlockExec) {
@@ -94,7 +96,7 @@ class MarkdownParser {
             }
 
             const [wholeCodeBlock, codeBlockContent] = codeBlockExec
-            htmlResult.push(`<pre><code>${codeBlockContent}</code></pre>`)
+            htmlResult.push(`<pre data-line='${index}'><code>${codeBlockContent}</code></pre>`)
             continue
          }
 
@@ -111,13 +113,13 @@ class MarkdownParser {
 
             // start fresh ul
             if (!isULRunning) {
-               htmlResult.push("<ul>")
+               htmlResult.push(`<ul data-line='${index}'>`)
                isULRunning = true
             }
 
             const [wholeUl, ulContent] = ulExec
             let parsedUlContent = this.parseAllInlineElementsWithinAnElement([ulContent])
-            htmlResult.push(`<li>${parsedUlContent}</li>`)
+            htmlResult.push(`<li data-line='${index}'>${parsedUlContent}</li>`)
          }
          else if (olExec) {
             // if ul-running, then close it off
@@ -128,13 +130,13 @@ class MarkdownParser {
 
             // start fresh ol
             if (!isOLRunning) {
-               htmlResult.push("<ol>")
+               htmlResult.push(`<ol data-line='${index}'>`)
                isOLRunning = true
             }
 
             const [wholeOl, olContent] = olExec
             let parsedOlContent = this.parseAllInlineElementsWithinAnElement([olContent])
-            htmlResult.push(`<li>${parsedOlContent}</li>`)
+            htmlResult.push(`<li data-line='${index}'>${parsedOlContent}</li>`)
          } else {
             if (isOLRunning) {
                htmlResult.push("</ol>")
@@ -152,11 +154,11 @@ class MarkdownParser {
                let parsedHeadingContent = this.parseAllInlineElementsWithinAnElement([headingContent])
 
                if (headingVariant == '#') {
-                  htmlResult.push(`<h1>${parsedHeadingContent}</h1>`)
+                  htmlResult.push(`<h1 data-line='${index}'>${parsedHeadingContent}</h1>`)
                } else if (headingVariant == "##") {
-                  htmlResult.push(`<h2>${parsedHeadingContent}</h2>`)
+                  htmlResult.push(`<h2 data-line='${index}'>${parsedHeadingContent}</h2>`)
                } else if (headingVariant == "###") {
-                  htmlResult.push(`<h3>${parsedHeadingContent}</h3>`)
+                  htmlResult.push(`<h3 data-line='${index}'>${parsedHeadingContent}</h3>`)
                }
                continue
             }
@@ -165,7 +167,7 @@ class MarkdownParser {
             const blockquoteExec = /^> (.+)$/g.exec(line)
             if (blockquoteExec) {
                const [wholeBlockquote, blockquoteContent] = blockquoteExec
-               htmlResult.push(`<blockquote>${blockquoteContent}</blockquote>`)
+               htmlResult.push(`<blockquote data-line='${index}'>${blockquoteContent}</blockquote>`)
                continue
             }
 
@@ -173,7 +175,7 @@ class MarkdownParser {
             // hr parsing
             const hrExec = /^[\s]*[-*_]{3,}[\s]*$/g.exec(line)
             if (hrExec) {
-               htmlResult.push("<hr />")
+               htmlResult.push(`<hr data-line='${index}'/>`)
                continue
             }
 
@@ -184,14 +186,14 @@ class MarkdownParser {
                const imgExec = /^!\[(.+?)\]\((.+?)\)/gm.exec(content)
                if (imgExec) {
                   const [wholeImg, imgAlt, imgSrc] = imgExec
-                  htmlResult.push(`<img src='${imgSrc}' alt='${imgAlt}' />`)
+                  htmlResult.push(`<img data-line='${index}' src='${imgSrc}' alt='${imgAlt}' />`)
                   continue
                }
 
                if (content.trim()) {
                   // parsing everything as paragraph
                   const paragraph = this.parseAllInlineElementsWithinAnElement([content])
-                  htmlResult.push(`<p>${paragraph}</p>`)
+                  htmlResult.push(`<p data-line='${index}'>${paragraph}</p>`)
                }
             }
          }
